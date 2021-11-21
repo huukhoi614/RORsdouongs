4b790ced69f133c426a6b7a8a4bf1f8128295194
nguyentri97@gmail.com <nguyentri97@gmail.com>
2021-11-21 22:21:41 +0700

    update database

--- AdminRORsdouongs/app/controllers/api/v1/sanphams_controller.rb
+++ AdminRORsdouongs/app/controllers/api/v1/sanphams_controller.rb
@@ -1,7 +1,6 @@
 module Api::V1
   class SanphamsController < ActionController::API
   
-
 	  def index
 	  	@sanphams =  Sanpham.select(:loaisp_id, :id, :tensanpham, :anh, :mota)
 	  	render json:  @sanphams, include: [:loaisp]
--- AdminRORsdouongs/app/models/ctspham.rb
+++ AdminRORsdouongs/app/models/ctspham.rb
@@ -3,8 +3,5 @@ class Ctspham < ApplicationRecord
   belongs_to :size
   has_many :ctchonthems
   has_many :banggia
-  validates :name, :presence => true
-  
 
-  
 end
