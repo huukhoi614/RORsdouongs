41f953772f6bb27c3c0bbe7a74379ee0acd119f9
nguyentri97@gmail.com <nguyentri97@gmail.com>
2021-12-15 11:20:24 +0700

    fix revert

--- RORsdouongs/config/initializers/assets.rb
+++ RORsdouongs/config/initializers/assets.rb
@@ -12,10 +12,3 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-
-Rails.application.config.assets.precompile += %w( bootstrap.min.css )
-Rails.application.config.assets.precompile += %w( elegent.min.css )
-Rails.application.config.assets.precompile += %w( font-awesome.min.css )
-Rails.application.config.assets.precompile += %w( helper.css )
-Rails.application.config.assets.precompile += %w( plugins.css )
-Rails.application.config.assets.precompile += %w( main.css )
