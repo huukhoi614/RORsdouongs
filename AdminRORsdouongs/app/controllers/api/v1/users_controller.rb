8cfd1793620bd3d901a7cdcff02480efef8c0234
nguyentri97@gmail.com <nguyentri97@gmail.com>
2021-12-23 14:55:53 +0700

    error create user

--- AdminRORsdouongs/app/controllers/api/v1/users_controller.rb
+++ AdminRORsdouongs/app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,33 @@
 module Api::V1
   class UsersController < ActionController::API
 
-  def index
-    @users = User.select(:id,:name,:phone,:hashed_password,:salt,:point)
-    render json: @users
-  end
+    def index
+      @users = User.select(:id,:name,:phone,:hashed_password,:salt,:point)
+      render json: @users
+    end
 
-  
+    def new
+      @user = User.new
+    end
+
+    def create
+      user = User.create(
+        {
+          name: params[:name],
+          phone: params[:phone],
+          hashed_password: params[:hashed_password],
+          password_confirmation: params[:password_confirmation],
+          point: params[:point],
+          salt: params[:salt],
+          password: params[:password],
+        }
+      )
+      if user.save
+        render json: "Created user success"
+      else
+        render json: "Cretate user failed"
+      end
+    end
 	
 	end
 end
