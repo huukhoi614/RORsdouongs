88ffd74133f7c0dd44aa983b72558be8c110df43
nguyentri7991 <54176457+nguyentri7991@users.noreply.github.com>
2022-01-06 16:45:02 +0700

    tri-api:fix khuyenmai sanpham

--- AdminRORsdouongs/app/controllers/api/v1/sanphams_controller.rb
+++ AdminRORsdouongs/app/controllers/api/v1/sanphams_controller.rb
@@ -4,18 +4,19 @@ module Api::V1
 	  def index
 		  @records = Sanpham
 			.joins("
-					left join ctsphams ON ctsphams.sanpham_id = sanphams.id 
-					left join banggia on banggia.ctspham_id = ctsphams.id
-					left join ctkhuyenmais on ctkhuyenmais.sanpham_id = sanphams.id
-					where ctsphams.size_id = 1
-				")
+				left join ctsphams ON ctsphams.sanpham_id = sanphams.id and ctsphams.size_id = 1
+				left join banggia on banggia.ctspham_id = ctsphams.id
+				left join ctkhuyenmais on ctkhuyenmais.sanpham_id = sanphams.id
+				left join khuyenmais on khuyenmais.id = ctkhuyenmais.khuyenmai_id and #{DateTime.current.to_date} BETWEEN khuyenmais.ngayBD and khuyenmais.ngayKT")
 			.select("sanphams.id,sanphams.tensanpham,sanphams.anh,banggia.gia,COALESCE(ctkhuyenmais.tylegiam,0) as discount")
-		render json: @records 
+		render json: @records
 	  end
 
 	  def show
 		@sanpham = Sanpham
-			.joins("left join ctkhuyenmais on ctkhuyenmais.sanpham_id = sanphams.id")
+			.joins("
+				left join ctkhuyenmais on ctkhuyenmais.sanpham_id = sanphams.id
+				left join khuyenmais on khuyenmais.id = ctkhuyenmais.khuyenmai_id and #{DateTime.current.to_date} BETWEEN khuyenmais.ngayBD and khuyenmais.ngayKT")
 			.select("sanphams.id,sanphams.tensanpham,sanphams.anh,COALESCE(ctkhuyenmais.tylegiam,0) as discount")
 			.find(params[:id])
 			render json: @sanpham
